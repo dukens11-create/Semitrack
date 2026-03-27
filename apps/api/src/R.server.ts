56ab3988d6264af59afa69bbeba89df89227d171
Dukens <dukens11@gmail.com>
2026-03-27 11:11:26 -0700

    Add initial R.server.ts with route imports

---
+++ apps/api/src/R.server.ts
@@ -0,0 +1,7 @@
+import { loadRouter } from "./modules/loads/load.routes.js";
+import { dispatchRouter } from "./modules/dispatch/dispatch.routes.js";
+import { tripStatusRouter } from "./modules/trips/trip-status.routes.js";
+import { notificationRouter } from "./modules/notifications/notification.routes.js";
+import { analyticsRouter } from "./modules/analytics/analytics.routes.js";
+import { moderationRouter } from "./modules/admin/moderation.routes.js";
+import { documentRouter } from "./modules/documents/document.routes.js";
